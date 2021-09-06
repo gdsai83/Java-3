2754b622a38919e94509479375cba27f3d340253
Denis Gayvoronskiy <82875496+gdsai83@users.noreply.github.com>
2021-09-06 17:09:23 +0300

    Delete src/main/java directory

--- src/main/java/Apple.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Apple extends Fruit {
-    private final double weight=1.0;
-
-    public double getWeight() {
-        return weight;
-    }
-}
--- src/main/java/Orange.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Orange extends Fruit {
-    private final double weight=1.5;
-
-    public double getWeight() {
-        return weight;
-    }
-}
